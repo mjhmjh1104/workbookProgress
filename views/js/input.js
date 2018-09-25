fb9f0e17a7c6be5b206de6f6da79cdf01345dc3a
mjhmjh1104 <mjhmjh1104@naver.com>
2018-09-25 18:05:24 +0900

    support

--- views/js/input.js
+++ views/js/input.js
@@ -1,10 +1,18 @@
 var inputs = document.getElementsByTagName('input');
 
-Array.prototype.forEach.call(inputs, function(item) {
+if (ieVersion() >= 9) Array.prototype.forEach.call(inputs, function(item) {
   item.addEventListener('click', select);
 });
 
 function select(e) {
   this.select();
   this.setSelectionRange(0, this.value.length);
+}
+
+function ieVersion() {
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf("MSIE ");
+  if (msie > 0) return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
+  else if (navigator.userAgent.match(/Trident.*rv\:11\./)) return 11;
+  else return -1;
 }
